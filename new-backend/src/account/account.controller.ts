9a2c8f3b98aad2246267a536b7297cfd9e78435c
Howard Tai <howardtai86@gmail.com>
2020-03-14 23:43:24 +0800

    :man: update accountModule with auth

--- new-backend/src/account/account.controller.ts
+++ new-backend/src/account/account.controller.ts
@@ -4,30 +4,94 @@ import {
   Body,
   Get,
   Param,
-  ClassSerializerInterceptor,
+  Request,
   UseInterceptors,
+  UseGuards,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
+import { SetCookies, Cookies, ClearCookies } from '@nestjsplus/cookies';
+
 import { AccountService } from './account.service';
 import { Account } from './account.entity';
-import { CreateAccountDto } from './dto/create-account.dto';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
+// TODO: Create custom cookie guard
 @Controller('account')
 export class AccountController {
-  constructor(private readonly accountService: AccountService) {}
+  constructor(
+    private readonly accountService: AccountService,
+    private readonly authService: AuthService,
+  ) {}
 
-  @Post('signup')
-  create(@Body() createAccountDto: CreateAccountDto): Promise<Account> {
-    return this.accountService.create(createAccountDto);
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Post('login')
+  @SetCookies()
+  async login(@Request() req) {
+    const { username, password } = req.body;
+    const account = await this.authService.validateLocal(username, password);
+
+    let jwt: string = '';
+    if (account) {
+      const cookies = this.authService.generateJwt(account);
+      (req as any)._cookies = cookies;
+      jwt = cookies[0].value;
+    }
+
+    return { ...account, jwt };
   }
 
   @UseInterceptors(ClassSerializerInterceptor)
-  @Get()
+  @Post('signup')
+  async create(@Request() req) {
+    const { username, password } = req.body;
+    const account = await this.accountService.create(username, password);
+    const cookies = this.authService.generateJwt(account);
+    (req as any)._cookies = cookies;
+    const jwt = cookies[0].value;
+    return { ...account, jwt };
+  }
+
+  @ClearCookies('jwt')
+  @Get('logout')
+  logout() {
+    return 'done';
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('users')
   findAll(): Promise<Account[]> {
     return this.accountService.findAll();
   }
 
-  @Get(':username')
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('users/:username')
   findOne(@Param('username') username: string): Promise<Account> {
     return this.accountService.findOne(username);
+  }
+
+  // ! To be deprecated
+  @Get('/authenticated')
+  async authenticated(@Cookies() cookies): Promise<{ authenticated: boolean }> {
+    const account = await this.authService.decodeCookies(cookies);
+    const authenticated = account ? true : false;
+    return { authenticated };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('dragons')
+  async accountDragons(@Cookies() cookies) {
+    const account = await this.authService.decodeCookies(cookies);
+    return account?.dragons;
+  }
+
+  // ! To be deprecated
+  @Get('info')
+  async info(@Cookies() cookies) {
+    const account = await this.authService.decodeCookies(cookies);
+    const { dragons, ...result } = account;
+    return result;
   }
 }
--- new-backend/src/account/account.module.ts
+++ new-backend/src/account/account.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from './account.entity';
 import { AccountService } from './account.service';
 import { AccountController } from './account.controller';
+import { AuthModule } from 'src/auth/auth.module';
+// import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account])],
+  imports: [TypeOrmModule.forFeature([Account]), AuthModule],
   providers: [AccountService],
   controllers: [AccountController],
   exports: [AccountService],
