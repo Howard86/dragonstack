991dcb9833ea27819540e0c25f9b1b6110ef58d4
Howard Tai <howardtai86@gmail.com>
2020-07-11 09:00:01 +0800

    :art: add Dragon DTO
    Signed-off-by: Howard Tai <howardtai86@gmail.com>

--- spring-boot/src/main/java/com/dragonstack/controller/AccountController.java
+++ spring-boot/src/main/java/com/dragonstack/controller/AccountController.java
@@ -7,13 +7,14 @@ import com.dragonstack.model.dto.AccountInfoDTO;
 import com.dragonstack.model.entity.Account;
 import com.dragonstack.persistance.AccountRepository;
 import com.dragonstack.util.Parser;
+import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/accounts")
 public class AccountController {
@@ -21,15 +22,7 @@ public class AccountController {
     private final AccountRepository accountRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final ModelMapper modelMapper;
-
-    @Autowired
-    private YAMLConfig config;
-
-    public AccountController(AccountRepository accountRepository, BCryptPasswordEncoder bCryptPasswordEncoder, ModelMapper modelMapper) {
-        this.accountRepository = accountRepository;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-        this.modelMapper = modelMapper;
-    }
+    private final YAMLConfig config;
 
 //    @PostMapping("/login") is at security.WebSecurity
 
@@ -44,21 +37,21 @@ public class AccountController {
         Account account = convertToEntity(accountCreationDTO);
         account.setPassword(bCryptPasswordEncoder.encode(account.getPassword()));
         accountRepository.save(account);
-        return new ResponseEntity<>(covertToDTO(account), HttpStatus.CREATED);
+        return new ResponseEntity<>(convertToDTO(account), HttpStatus.CREATED);
     }
 
     @GetMapping("/info")
     public ResponseEntity<AccountInfoDTO> getInfo(@RequestHeader(SecurityConstants.HEADER_STRING) String header) {
         String username = Parser.parseJWTHeader(header, config.getSecret());
         Account account = accountRepository.findByUsername(username);
-        return new ResponseEntity<>(covertToDTO(account), HttpStatus.OK);
+        return new ResponseEntity<>(convertToDTO(account), HttpStatus.OK);
     }
 
     private Account convertToEntity(AccountCreationDTO accountCreationDTO) {
         return modelMapper.map(accountCreationDTO, Account.class);
     }
 
-    private AccountInfoDTO covertToDTO(Account account) {
+    private AccountInfoDTO convertToDTO(Account account) {
         return modelMapper.map(account, AccountInfoDTO.class);
     }
 }
--- spring-boot/src/main/java/com/dragonstack/controller/DragonController.java
+++ spring-boot/src/main/java/com/dragonstack/controller/DragonController.java
@@ -2,6 +2,7 @@ package com.dragonstack.controller;
 
 import com.dragonstack.YAMLConfig;
 import com.dragonstack.constant.SecurityConstants;
+import com.dragonstack.model.dto.DragonInfoDTO;
 import com.dragonstack.model.dto.DragonUpdateDTO;
 import com.dragonstack.model.entity.Account;
 import com.dragonstack.model.entity.Dragon;
@@ -11,12 +12,14 @@ import com.dragonstack.util.Parser;
 import javassist.NotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/dragons")
@@ -27,31 +30,33 @@ public class DragonController {
     private final DragonService dragonService;
     private final AccountRepository accountRepository;
     private final YAMLConfig config;
+    private final ModelMapper modelMapper;
 
     @GetMapping
-    public ResponseEntity<List<Dragon>> getOwnedDragons(@RequestHeader(SecurityConstants.HEADER_STRING) String header) {
+    public ResponseEntity<List<DragonInfoDTO>> getOwnedDragons(@RequestHeader(SecurityConstants.HEADER_STRING) String header) {
         Account account = getAccount(header);
         List<Dragon> dragons = dragonService.getOwnedDragons(account);
-        return new ResponseEntity<>(dragons, HttpStatus.OK);
+        return new ResponseEntity<>(convertToDTOs(dragons), HttpStatus.OK);
     }
 
     @GetMapping("/new")
-    public ResponseEntity<Dragon> getNewDragon(@RequestHeader(SecurityConstants.HEADER_STRING) String header) {
+    public ResponseEntity<DragonInfoDTO> getNewDragon(@RequestHeader(SecurityConstants.HEADER_STRING) String header) {
         Account account = getAccount(header);
         Dragon dragon = dragonService.create(account);
-        return new ResponseEntity<>(dragon, HttpStatus.CREATED);
+        return new ResponseEntity<>(convertToDTO(dragon), HttpStatus.CREATED);
     }
 
     @GetMapping("/public")
-    public ResponseEntity<List<Dragon>> getPublicDragons() {
-        return new ResponseEntity<>(dragonService.getPublicDragons(), HttpStatus.OK);
+    public ResponseEntity<List<DragonInfoDTO>> getPublicDragons() {
+        List<Dragon> dragons = dragonService.getPublicDragons();
+        return new ResponseEntity<>(convertToDTOs(dragons), HttpStatus.OK);
     }
 
-    @PutMapping("")
-    public ResponseEntity<Dragon> updateDragon(
+    @PutMapping()
+    public ResponseEntity<DragonInfoDTO> updateDragon(
             @RequestBody DragonUpdateDTO dragonUpdateDTO,
             @RequestHeader(SecurityConstants.HEADER_STRING) String header
-    ) throws NotFoundException {
+    ) throws NotFoundException, IllegalAccessException {
 
         Long id = dragonUpdateDTO.getId();
         Optional<Dragon> dragonOptional = dragonService.getOne(id);
@@ -61,39 +66,31 @@ public class DragonController {
         }
 
         Dragon dragon = dragonOptional.get();
+        Long ownerId = dragon.getAccount().getId();
+        Long changerId = getAccount(header).getId();
 
-// TODO: fix that dragon.getAccount is null
-//        Long ownerId = dragon.getAccount().getId();
-//        log.info("ownerId is {}", ownerId);
-//        Long changerId = getAccount(header).getId();
-//        log.info("changerId is {}", changerId);
-//        if (!ownerId.equals(changerId)) {
-//            throw new IllegalAccessException("dragon with id " + id + " is not yours");
-//        }
-
-
-//        TODO: improve this implementation with Object model
-        if (dragonUpdateDTO.getNickname() != null) {
-            dragon.setNickname(dragonUpdateDTO.getNickname());
-        }
-        if (dragonUpdateDTO.getSaleValue() != null) {
-            dragon.setSaleValue(dragonUpdateDTO.getSaleValue());
-        }
-        if (dragonUpdateDTO.getSireValue() != null) {
-            dragon.setSireValue(dragonUpdateDTO.getSireValue());
-        }
-        if (dragonUpdateDTO.getIsPublic() != null) {
-            dragon.setPublic(dragonUpdateDTO.getIsPublic());
-        }
-        if (dragonUpdateDTO.getAccount() != null) {
-            dragon.setAccount(dragonUpdateDTO.getAccount());
+        if (!ownerId.equals(changerId)) {
+            throw new IllegalAccessException("dragon with id " + id + " is not yours");
         }
 
-        return new ResponseEntity<>(dragonService.update(dragon), HttpStatus.OK);
+        modelMapper.getConfiguration().setSkipNullEnabled(true);
+        modelMapper.map(dragonUpdateDTO, dragon);
+
+        Dragon updatedDragon = dragonService.update(dragon);
+
+        return new ResponseEntity<>(convertToDTO(updatedDragon), HttpStatus.OK);
     }
 
     private Account getAccount(@RequestHeader(SecurityConstants.HEADER_STRING) String header) {
         String username = Parser.parseJWTHeader(header, config.getSecret());
         return accountRepository.findByUsername(username);
+    }
+
+    private DragonInfoDTO convertToDTO(Dragon dragon) {
+        return modelMapper.map(dragon, DragonInfoDTO.class);
+    }
+
+    private List<DragonInfoDTO> convertToDTOs(List<Dragon> dragons) {
+        return dragons.stream().map(this::convertToDTO).collect(Collectors.toList());
     }
 }
